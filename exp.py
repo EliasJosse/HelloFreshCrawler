7785ec37bd6cf3f8eff7b7dd1421adf25aa221d8
unknown <eliasjosse@gmail.com>
2022-11-06 21:45:40 +0100

    Title, ingredients and steps done

--- exp.py
+++ exp.py
@@ -6,9 +6,8 @@ import requests
 from bs4 import BeautifulSoup
 
 
-
+#Gather all recipe links on page
 def getRecipeLinks(soup) :
-    #Gather all links on page
     links = [tag_a.get('href') for tag_a in soup.find_all('a')]
 
     #recipe links end with  '-' + <24 hex-digits>
@@ -17,6 +16,37 @@ def getRecipeLinks(soup) :
 
     return recipe_links
 
+#Extract Title, description, ingredients, amount, and steps.
+def getTitle_Ingredients_Steps(recipe_link) :
+    recipe_page = requests.get(recipe_link)
+
+    recipe_page_soup = BeautifulSoup(recipe_page.content, "html.parser")
+
+    title = recipe_page_soup.find('h1').text
+
+    tags_p = recipe_page_soup.find_all('p')
+
+    description = tags_p[1]
+
+    tags_p.remove(tags_p[0])
+    tags_p.remove(tags_p[0])
+
+    ingredients = []
+    steps = []
+
+    for tag_p in tags_p :
+
+        
+        # Ingredients
+        try :
+            if type(tag_p['class']) :
+                ingredients.append(tag_p.text)
+            
+        # Steps
+        except :
+            steps.append(tag_p.text)
+    
+    return title, description, ingredients, steps
 
 
 
@@ -28,35 +58,30 @@ soup = BeautifulSoup(page.content, "html.parser")
 
 recipe_links = getRecipeLinks(soup)
 
-
-
-
-
-#Extract Title, subttitle, ingredients, amount and steps.
-
 recipe_page = requests.get(recipe_links[1])
 
 recipe_page_soup = BeautifulSoup(recipe_page.content, "html.parser")
 
-title = recipe_page_soup.find('h1').text
+isNutrient = False
+for tag_span in recipe_page_soup.find_all('span') :
 
-tags_p = recipe_page_soup.find_all('p')
+    if tag_span.text == "Näringsvärden" :
+        isNutrient = True
 
-description = tags_p[1]
+    if tag_span.text == "Köksredskap" :
+        isNutrient = False
 
-tags_p.remove(tags_p[0])
-tags_p.remove(tags_p[0])
+    if isNutrient :
+        print(tag_span)
 
-for tag_p in tags_p :
 
-    
-    # Ingredients
-    try :
-        if type(tag_p['class']) :
 
-    # Steps
-    except :
-        
+
+# Extract duration and nutritional value.
+
+
+
+
 
 #file = open(title, 'w')
 #file.write(title)
